06f7d96b6e3d3dd32a7149dc0a3020cce8501e69
medhatjachour <53466991+medhatjachour@users.noreply.github.com>
2023-03-18 20:35:56 +0200

    main

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,14 +1,22 @@
 from app import app_context
 from app import MainWindow
+from PySide6.QtWidgets import QMessageBox
 import sys
-id = 0
-import wmi
-c = wmi.WMI()
+import subprocess
+x = (subprocess.run("wmic diskdrive get serialnumber",  capture_output=True)).stdout
+# id = int( str( x[21:37])[2:-1] )
+id = (str( x[21:37])[2:-1]) 
 if __name__ == '__main__':       # 1. Instantiate ApplicationContext
-    for item in c.Win32_PhysicalMedia():
-        id  = item.SerialNumber
-        if id == "50026B73811921F3":
-            window = MainWindow()
-            window.show()
-            exit_code = app_context.app.exec()      # 2. Invoke appctxt.app.exec_()
-            sys.exit(exit_code)
+    
+    if id == "H2UBTY010211017":
+    # if id == "50026B73811921F3":
+        window = MainWindow()
+        window.show()
+        exit_code = app_context.app.exec()      # 2. Invoke appctxt.app.exec_()
+        sys.exit(exit_code)
+    else:
+        dlg = QMessageBox()
+        dlg.setWindowTitle("It's not your Version")
+        dlg.setText("this version is not authorized to run on this pc so تعالي لبابا يسطي\n the website is : medhatjachour.com")
+        dlg.setIcon(QMessageBox.Critical)
+        dlg.exec()
